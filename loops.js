2df44faa91c1300f03a4d4f9ced56956b7928a3a
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-02-11 03:24:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,11 +17,10 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(num) {
-    function incrementVariable () {
-      num = num + 1;
-      return num;
-    }
-    do {
-      console.log("I run once regardless.");
-    } while(incrementVariable() < num);
+  num = num + 1
+  return num;
 }
+
+do {
+      console.log("I run once regardless.");
+    } while(doWhileloop() < num);
