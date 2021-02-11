951b03540b914c3655a42f3d0c357a5c15e5df1c
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-02-11 02:42:21 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,12 +1,10 @@
 function forLoop(array) {
   for (let i=0; i<25; i++) {
-    if (i === 0) {
-      array.push("I am 0 strange loop.")
-      else if (i === 1) {
+    if (i === 1) {
       array.push("I am 1 strange loop.")
     } else {
       array.push(`I am ${i} strange loop.`)
     }
-    return array;
   }
+  return array
 }
